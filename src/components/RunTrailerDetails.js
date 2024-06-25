ec1d62a530282f619a67407c070f5065be709581
bala manohar <balamanohar.yeminedi@gmail.com>
2024-06-25 13:12:08 +0530

    play trailer in new page

--- src/components/RunTrailerDetails.js
+++ src/components/RunTrailerDetails.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-const RunTrailerDetails = ({ title, description }) => {
+const RunTrailerDetails = ({ title, description, movieID, rating }) => {
   const [isMoreActive, setIsMoreActive] = useState(false);
   const [overview, setOverview] = useState(description.slice(0, 190));
 
@@ -8,20 +9,31 @@ const RunTrailerDetails = ({ title, description }) => {
     setIsMoreActive(!isMoreActive);
     if (isMoreActive) {
       setOverview(description.slice(0, 190));
-    } else {  
+    } else {
       setOverview(description.slice(0, 1900));
     }
   };
   return (
     <div className=" absolute  bg-gradient-to-r w-screen aspect-video from-black py-48 px-8 text-white">
       <h2 className=" font-semibold text-4xl">{title}</h2>
-      <p className={isMoreActive ? "w-1/2 py-4" : "w-1/3 py-4"}>
+      <p className={isMoreActive ? "w-1/2 py-6" : "w-1/3 py-6"}>
         {overview}
         {!isMoreActive && description.length > 190 && "..."}
       </p>
-      <button className="rounded-md bg-white text-black px-8 py-2 mr-3 hover:bg-opacity-80">
+
+      <Link
+        className="rounded-md bg-white text-black px-8 py-2 mr-3 hover:bg-opacity-80"
+        to={"/movie/" + movieID}
+        state={{
+          id: movieID,
+          title: title,
+          overview: description,
+          vote_average: rating,
+        }}
+      >
         Play
-      </button>
+      </Link>
+
       {overview.length >= 190 && (
         <button
           className="rounded-md bg-white text-white px-8 py-2 bg-opacity-20 "
