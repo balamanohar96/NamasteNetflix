b835671418b4815bbb2bca751e80a1cad5be4a52
bala manohar <balamanohar.yeminedi@gmail.com>
2024-07-03 07:14:48 +0530

    footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="text-center bg-slate-200 p-6 leading-tight">
+      <h2>©Bala Manohar.</h2>
+      <h2>mail : balamanohar.yeminedi@gmail.com</h2>
+      <h2>phone : 8754539642</h2>
+    </div>
+  );
+};
+
+export default Footer;
