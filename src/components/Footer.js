3be0761469ae485bcb73522e430f85ebf3bba9fe
bala manohar <balamanohar.yeminedi@gmail.com>
2024-07-30 11:56:47 +0530

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="text-center bg-slate-200 p-6 leading-tight">
-      <h2>©Bala Manohar.</h2>
-      <h2>mail : balamanohar.yeminedi@gmail.com</h2>
-      <h2>phone : 8754539642</h2>
+    <div className="text-center text-white bg-black p-6 leading-tight">
+      <h2>© Bala Manohar</h2>
+      <h2>mail-balamanohar.yeminedi@gmail.com</h2>
+      <h2>phone-8754539642</h2>
     </div>
   );
 };
